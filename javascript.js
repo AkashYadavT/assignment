02ef0db503116c9c0088db4fc7b696821c21914d
AkashYadavT <aksah795515@gmail.com>
2023-11-23 14:30:16 +0530

    initial commit

---
+++ javascript.js
@@ -0,0 +1,4 @@
+var tittle = document.getElementById("tittle");
+var director = document.getElementById("Director");
+var stars = Document.getElementById("stars");
+var platform = document.getElementById("Platform");
